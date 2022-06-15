e200b49c9ebd16ebfd27accbe2d1dbc247a88bfe
Natnael Ghirma <natnael.g@suntecholding.com>
2022-06-15 17:21:40 +0300

    updated query

--- app/Http/Services/ReportSummaryService.php
+++ app/Http/Services/ReportSummaryService.php
@@ -53,4 +53,23 @@ class ReportSummaryService
                 "project_types.id"
             )->get();
     }
+
+    public function getProjectMilestone($project_id)
+    {
+        return \DB::table("projects")
+            ->join("tasks", "projects.id", "=", "tasks.project_id")
+            ->join("project_types", "projects.project_type_id", "=", "project_types.id")
+            ->where("projects.id", $project_id)
+            ->select(
+                \DB::raw('SUM(CASE WHEN status = "closed" THEN 1 ELSE 0 END) as closed'),
+                \DB::raw('SUM(CASE WHEN status IS NOT NULL THEN 1 ELSE 0 END) as total'),
+                \DB::raw('ROUND(SUM(CASE WHEN status = "closed" THEN 1 ELSE 0 END) / SUM(CASE WHEN status IS NOT NULL THEN 1 ELSE 0 END) * 100,2) AS total_progress'),
+                "project_types.title as project_type_name",
+                "project_types.id as project_type_id"
+            )
+            ->groupBy(
+                "project_types.title",
+                "project_types.id"
+            )->get();
+    }
 }
