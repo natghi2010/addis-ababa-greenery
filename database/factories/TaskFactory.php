745dcd755f65d22f8ae70708d5584dae6cb4aa98
Natnael Ghirma <natnael.g@suntecholding.com>
2022-07-05 10:18:21 +0300

    updated tasks

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -14,9 +14,9 @@ class TaskFactory extends Factory
     public function definition()
     {
         return [
-            "title"=>$this->faker->word,
-            "description"=>$this->faker->text,
-            "status"=>$this->faker->randomElement(["pending","open","in progress","closed"]),
+            "title" => $this->faker->randomElement(['task 1', 'task 2', 'task 3', 'task 4', 'task 5', 'task 6', 'task 7', 'task 8', 'task 9', 'task 10']),
+            "description" => $this->faker->text,
+            "status" => $this->faker->randomElement(["pending", "in progress", "closed"]),
         ];
     }
 }
