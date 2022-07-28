b225108e713e3265085f5b9cce35f81795accb0b
Natnael Ghirma <natnael.g@suntecholding.com>
2022-07-28 16:58:34 +0300

    updated reporting

--- app/Http/Services/MilestoneService.php
+++ app/Http/Services/MilestoneService.php
@@ -30,4 +30,26 @@ class MilestoneService
     {
         Milestone::find($id)->delete();
     }
+
+    public function getMilestonesByProject($id)
+    {
+        return \DB::table("milestones")
+            ->join("tasks", "milestone_id", "=", "milestones.id")
+            ->where("milestones.project_id", $id)
+            ->select(
+                "milestones.name",
+                "milestones.start_date",
+                "milestones.end_date",
+                //count all closed tasks
+                \DB::raw('SUM(CASE WHEN status = "closed" THEN 1 ELSE 0 END) as closed'),
+                \DB::raw('SUM(CASE WHEN status IS NOT NULL THEN 1 ELSE 0 END) as total'),
+                \DB::raw('ROUND(SUM(CASE WHEN status = "closed" THEN 1 ELSE 0 END) / SUM(CASE WHEN status IS NOT NULL THEN 1 ELSE 0 END) * 100,2) AS total_progress'),
+            )
+            ->groupBy(
+                "milestones.name",
+                "milestones.start_date",
+                "milestones.end_date"
+            )   
+            ->get();
+    }
 }
